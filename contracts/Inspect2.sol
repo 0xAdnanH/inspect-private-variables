d9d7052f97c710a32b532af8aa5848b5992646ad
0xAdnanH <123894765+0xAdnanH@users.noreply.github.com>
2023-05-31 13:00:08 +0300

    Inspect2.sol

--- contracts/Inspect2.sol
+++ contracts/Inspect2.sol
@@ -1,14 +1,26 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+/**
+ * @title Inspect2
+ * @dev A contract for inspecting private address and uint96 variables.
+ */
 contract Inspect2 {
     address private testAddress;
     uint96 private testNumber;
 
+    /**
+     * @dev Sets the value of the private testAddress variable.
+     * @param _testAddress The address to be set.
+     */
     function setAddress(address _testAddress) public {
         testAddress = _testAddress;
     }
 
+    /**
+     * @dev Sets the value of the private testNumber variable.
+     * @param _testNumber The uint96 value to be set.
+     */
     function setNumber(uint96 _testNumber) public {
         testNumber = _testNumber;
     }
